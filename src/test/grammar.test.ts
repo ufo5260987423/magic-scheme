448dec5f0f858c8cb576d18dbd5fc78cc9375385
ufo5260987423 <ufo5260987423@163.com>
2026-06-12 08:55:16 +0800

    fix(grammar): add R6RS open-input-output-file
    - Add open-input-output-file to the port/file procedure list in
      support.function.general.
    - Add a regression test for open-input-output-file classification.
    
    Refs: Chez Scheme documentation review

--- src/test/grammar.test.ts
+++ src/test/grammar.test.ts
@@ -732,6 +732,7 @@ suite('Grammar Tokenization', () => {
         assertTokenScope('(char-foldcase #A)', 'char-foldcase', 'support.function.general');
         assertTokenScope('(bytevector-s16-ref bv 0 (endianness little))', 'bytevector-s16-ref', 'support.function.general');
         assertTokenScope('(bytevector-ieee-double-native-ref bv 0)', 'bytevector-ieee-double-native-ref', 'support.function.general');
+        assertTokenScope('(open-input-output-file "foo")', 'open-input-output-file', 'support.function.general');
     });
 
 });
