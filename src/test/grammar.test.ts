9418b43a213b829ca38a73ebd55e11a96bbeeaf0
ufo5260987423 <ufo5260987423@163.com>
2026-06-12 08:31:55 +0800

    fix(grammar): reclassify raise as a procedure
    - Move raise from keyword.control to support.function.general, matching
      the R6RS classification of raise as a procedure.
    - Add a regression test for raise classification.
    
    Refs: Chez Scheme documentation review round 19

--- src/test/grammar.test.ts
+++ src/test/grammar.test.ts
@@ -674,6 +674,7 @@ suite('Grammar Tokenization', () => {
         assertTokenScope('(library-requirements (foo))', 'library-requirements', 'support.function.general');
         assertTokenScope('(make-hash-table)', 'make-hash-table', 'support.function.general');
         assertTokenScope('(ftype-pointer-address p)', 'ftype-pointer-address', 'support.function.general');
+        assertTokenScope('(raise exn)', 'raise', 'support.function.general');
         assertTokenScope('(raise-continuable exn)', 'raise-continuable', 'support.function.general');
         assertTokenScope('(assertion-violation \'who "msg")', 'assertion-violation', 'support.function.general');
         assertTokenScope('(syntax-violation \'who "msg" form)', 'syntax-violation', 'support.function.general');
